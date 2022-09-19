aa548f0f4ec389388c1ec6ba58c4074d2596a7fa
Pcrab <pcrab@pcrab.xyz>
2022-09-19 10:50:21 +0800

    update template

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "jest";
+
+const config: Config = {
+    preset: "ts-jest/presets/default-esm",
+    moduleNameMapper: {
+        "^(\\.{1,2}/.*)\\.js$": "$1",
+    },
+    testMatch: ["**/__tests__/**/*.+(ts)", "**/?(*.)+(spec|test).+(ts)"],
+    collectCoverage: true,
+    transform: {
+        "<regex_match_files": [
+            "ts-jest",
+            {
+                useESM: true,
+            },
+        ],
+    },
+};
+
+export default config;
