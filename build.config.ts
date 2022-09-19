f3bdf5666cb68020596ed7bca965c4ea22c82894
Pcrab <pcrab@pcrab.xyz>
2022-09-19 16:57:56 +0800

    update format

--- build.config.ts
+++ build.config.ts
@@ -1,15 +1,15 @@
-import { defineBuildConfig } from 'unbuild';
+import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
     // If entries is not provided, will be automatically inferred from package.json
     entries: [
         // default
-        './src/index'
+        "./src/index",
     ],
     // Generates .d.ts declaration file
     declaration: true,
     clean: true,
     rollup: {
-        emitCJS: true
-    }
+        emitCJS: true,
+    },
 });
